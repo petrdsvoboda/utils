685842e7b35c4712264378c4094169c743780b4b
Petr D. Svoboda <petrdsvoboda@gmail.com>
2019-09-10 17:28:13 +0200

    Also works?

--- src/record.ts
+++ src/record.ts
@@ -1,25 +1,24 @@
 export type Map<T> = { [key in string]: T }
 
-export function get<T extends Map<any>, K1 extends keyof T>(
-	map: T | undefined,
-	path: [K1]
-): T[K1] | undefined
-export function get<
-	T extends Map<any>,
-	K1 extends keyof T,
-	K2 extends keyof T[K1]
->(map: T | undefined, path: [K1, K2]): T[K1][K2] | undefined
-export function get<
-	T extends Map<any>,
-	K1 extends keyof T,
-	K2 extends keyof T[K1]
->(map: T | undefined, path: [K1] | [K1, K2]) {
-	const [id, ...rest] = path
-	if (map === undefined) return undefined
+export function get<T extends Map<any>>(
+	map: T | undefined
+): <K1 extends keyof T>(path: [K1]) => T[K1] | undefined
+export function get<T extends Map<any>>(
+	map: T | undefined
+): <K1 extends keyof T, K2 extends keyof T[K1]>(
+	path: [K1, K2]
+) => T[K1][K2] | undefined
+export function get<T extends Map<any>>(map: T | undefined) {
+	return <K1 extends keyof T, K2 extends keyof T[K1]>(
+		path: [K1] | [K1, K2]
+	) => {
+		const [id, ...rest] = path
+		if (map === undefined) return undefined
 
-	const value = map[id]
-	if (rest === undefined || rest.length === 0) return value
-	return get(value, rest)
+		const value = map[id]
+		if (rest === undefined || rest.length === 0) return value
+		return get(value)(rest)
+	}
 }
 
 export function update<T extends Map<any>, K1 extends keyof T>(
