44ebeab3e4bfbc17253243b20e082bfcc26db2c8
Petr D. Svoboda <petrdsvoboda@gmail.com>
2019-10-15 16:52:52 +0200

    Build lib

--- lib/array.d.ts
+++ lib/array.d.ts
@@ -4,10 +4,10 @@ declare type CompareOptions = {
     isDate?: boolean;
 };
 export declare function sort<T extends Map<any>, K1 extends keyof T>(array: T[], options: CompareOptions, key1: K1): T[];
-export declare function sort<T extends Map<any>, K1 extends keyof T, K2 extends keyof T[K1]>(array: T[], options: CompareOptions, key1: K1, key2: K2): T[];
-export declare function sort<T extends Map<any>, K1 extends keyof T, K2 extends keyof T[K1], K3 extends keyof T[K1][K2]>(array: T[], options: CompareOptions, key1: K1, key2: K2, key3: K3): T[];
-export declare function sort<T extends Map<any>, K1 extends keyof T, K2 extends keyof T[K1], K3 extends keyof T[K1][K2], K4 extends keyof T[K1][K2][K3]>(array: T[], options: CompareOptions, key1: K1, key2: K2, key3: K3, key4: K4): T[];
-export declare function sort<T extends Map<any>, K1 extends keyof T, K2 extends keyof T[K1], K3 extends keyof T[K1][K2], K4 extends keyof T[K1][K2][K3], K5 extends keyof T[K1][K2][K3][K4]>(array: T[], options: CompareOptions, key1: K1, key2: K2, key3: K3, key4: K4, key5: K5): T[];
+export declare function sort<T extends Map<any>, K1 extends keyof T, K2 extends keyof NonNullable<T[K1]>>(array: T[], options: CompareOptions, key1: K1, key2: K2): T[];
+export declare function sort<T extends Map<any>, K1 extends keyof T, K2 extends keyof NonNullable<T[K1]>, K3 extends keyof NonNullable<NonNullable<T[K1]>[K2]>>(array: T[], options: CompareOptions, key1: K1, key2: K2, key3: K3): T[];
+export declare function sort<T extends Map<any>, K1 extends keyof T, K2 extends keyof NonNullable<T[K1]>, K3 extends keyof NonNullable<T[K1][K2]>, K4 extends keyof NonNullable<T[K1][K2][K3]>>(array: T[], options: CompareOptions, key1: K1, key2: K2, key3: K3, key4: K4): T[];
+export declare function sort<T extends Map<any>, K1 extends keyof T, K2 extends keyof NonNullable<T[K1]>, K3 extends keyof NonNullable<T[K1][K2]>, K4 extends keyof NonNullable<T[K1][K2][K3]>, K5 extends keyof NonNullable<T[K1][K2][K3][K4]>>(array: T[], options: CompareOptions, key1: K1, key2: K2, key3: K3, key4: K4, key5: K5): T[];
 declare type MergeOptions = {
     unique?: boolean;
 };
