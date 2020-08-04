2fdb1499d15748df267a1655ecda33f8f225f012
Petr D. Svoboda <petrdsvoboda@gmail.com>
2020-08-04 15:07:26 +0200

    Add random

--- src/number.ts
+++ src/number.ts
@@ -6,3 +6,9 @@ export const round = (val: number, precision?: number): number => {
 	const order = Math.pow(10, precision ?? 0)
 	return Math.round(val * order) / order
 }
+
+export const random = (max: number, min = 0): number => {
+	min = Math.ceil(min)
+	max = Math.floor(max)
+	return Math.floor(Math.random() * (max - min + 1)) + min
+}
