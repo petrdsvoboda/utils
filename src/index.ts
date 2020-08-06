9d6b512210b07dafa632465550c28a5f6a46cf84
Petr D. Svoboda <petrdsvoboda@gmail.com>
2020-08-06 17:35:12 +0200

    Trying lenses

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,33 @@ export * as record from './record'
 export * as string from './string'
 export * as types from './types'
 
-import * as record from './record'
+const lens = <T extends Record<string, any>, TKey extends keyof T>(
+	key: TKey
+) => {
+	return (record: T) => (value: T[keyof T]): T => {
+		record[key] = value
+		return record
+	}
+}
 
-const test = { a: { b: 3 } }
-record.set('s', 'c')(4)(test)
+type Nested<T extends Record<string, any>> = T[]
+
+type Rec = Record<string, any>
+type Lens<
+	T extends Rec,
+	X extends unknown[] = Lens2<T[keyof T]>
+> = T[keyof T] extends null ? [] : [keyof T, ...X[]]
+type Lens2<T extends Rec> = [keyof T]
+
+const obj = {
+	foo: 1,
+	bar: {
+		barz: 2,
+		barzz: 3
+	}
+}
+
+const x: Lens<typeof obj> = ['bar', 'ff']
+
+const l = lens('bar')
+const res = l(obj)(23)
